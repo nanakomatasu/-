366eb84a6e9bbfcc7c8cffe016bb41fd0ea7c825
nanakomatasu <chendongdi6@gmail.com>
2023-03-02 09:45:40 +0800

    配置了vuex的模块

---
+++ src/store/modules/user.js
@@ -0,0 +1,14 @@
+const state = {};
+const getters = {};
+const mutations = {};
+const actions = {};
+const modules = {};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions,
+  modules,
+};
